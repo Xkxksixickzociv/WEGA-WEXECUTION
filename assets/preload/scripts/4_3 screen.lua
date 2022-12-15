ae12adab1579b5f6595aa969a536c5b9367cc814
Xkxksixickzociv <119426594+Xkxksixickzociv@users.noreply.github.com>
2022-12-15 16:33:48 +0200

    Add files via upload

---
+++ assets/preload/scripts/4_3 screen.lua
@@ -0,0 +1,40 @@
+local zoom = false --cam zooming
+
+function onCreatePost()
+ for i = 0, 3 do
+  if not middlescroll then
+   setPropertyFromGroup('strumLineNotes', i, 'x', getPropertyFromGroup('strumLineNotes', i, 'x') + 90)
+  end
+ end
+ 
+ for i = 4, 7 do
+  if not middlescroll then
+   setPropertyFromGroup('strumLineNotes', i, 'x', getPropertyFromGroup('strumLineNotes', i, 'x') - 70)
+  else
+   setPropertyFromGroup('strumLineNotes', i, 'x', getPropertyFromGroup('strumLineNotes', i, 'x') + 5)
+  end
+ end
+
+ for i = 2, 3 do
+  if middlescroll then
+   setPropertyFromGroup('strumLineNotes', i, 'x', getPropertyFromGroup('strumLineNotes', i, 'x') - 90)
+  end
+ end
+
+ for i = 0, 1 do
+  if middlescroll then
+   setPropertyFromGroup('strumLineNotes', i, 'x', getPropertyFromGroup('strumLineNotes', i, 'x') + 100)
+  end
+ end
+
+ makeLuaSprite('borderLeft', 'border', 0, 0); makeLuaSprite('borderRight', 'border', 1120, 0); makeLuaSprite('borderRig', 'border', 1300, 600);
+ addLuaSprite('borderLeft', false); addLuaSprite('borderRight', false)
+ setProperty('borderLeft.antialiasing', false); setProperty('borderRight.antialiasing', false)
+ setObjectCamera('borderLeft', 'camOther'); setObjectCamera('borderRight', 'camOther')
+end
+
+function onUpdatePost()
+ setProperty('camZooming', zoom) 
+end
+
+--script by KaoyDumb
